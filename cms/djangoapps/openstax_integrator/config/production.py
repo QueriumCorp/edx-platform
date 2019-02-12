6fe038119f22e73e28fb30838d0ef8e20a57c7ec
Lawrence McDaniel <lpm0073@gmail.com>
2019-02-12 11:39:20 -0600

    remove local dev modules and files

--- cms/djangoapps/openstax_integrator/config/production.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-import os
-from .common import Common
-
-
-class Production(Common):
-    INSTALLED_APPS = Common.INSTALLED_APPS
--- cms/djangoapps/openstax_integrator/users/admin.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-
-class UserAdmin(UserAdmin):
-    pass
-UserAdmin = admin.register(User)(UserAdmin)
