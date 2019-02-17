72252adf3f3409843e36201fffe6c4081b2475cb
Lawrence McDaniel <lpm0073@gmail.com>
2019-02-17 10:46:54 -0600

    add accounts_uuid__c, a salesforce 'External ID' field to enable use of sf.bulk.CampaignMember.upsert().

--- cms/djangoapps/openstax_integrator/salesforce/management/commands/sfpush.py
+++ cms/djangoapps/openstax_integrator/salesforce/management/commands/sfpush.py
@@ -31,46 +31,10 @@ class Command(BaseCommand):
         self.stdout.write(self.style.NOTICE(u"Begin updating salesforce.com CampaignMembers table ..."))
 
         u"""
-            data = [
-                  {'Id': '0000000000AAAAA', 'Email': 'examplenew2@example.com'},
-                  {'Email': 'foo@foo.com'}
-                ]
 
-            sf.bulk.CampaignMember.upsert(data, 'Id')
-
-            SELECT Email, CampaignID,
-                  ContactID,
-                  Initial_Sign_in_Date__c,
-                  Most_recent_sign_in_date__c,
-                  Completed_Training_Wheels_date__c,
-                  Started_Assignment_date__c,
-                  Completed_Assignment_date__c,
-                  Soft_Ask_Decision__c,
-                  Soft_Ask_Decision_date__c,
-                  Estimated_Enrollment__c,
-                  latest_adoption_decision__c FROM CampaignMember
-
-            OrderedDict(
-                [(u'totalSize', 2), (u'done', True),
-                 (u'records',
-                    [
-                        OrderedDict([(u'attributes',
-                        OrderedDict([(u'type', u'CampaignMember'),
-                                     (u'url', u'/services/data/v38.0/sobjects/CampaignMember/00v0m000000NgWYAA0')
-                                     ]
-                                     )),
-                                     (u'Email', u'denver@rice.edu'), (u'CampaignId', u'7010m0000002pARAAY'), (u'ContactId', u'0030m00000IzDI4AAN')]),
-
-                        OrderedDict([(u'attributes',
-                        OrderedDict([(u'type', u'CampaignMember'),
-                                     (u'url', u'/services/data/v38.0/sobjects/CampaignMember/00v0m000000NgWZAA0')])),
-                                     (u'Email', u'denver2@rice.edu'), (u'CampaignId', u'7010m0000002pARAAY'), (u'ContactId', u'0030m00000CMTuvAAH')])]
-                                     )
-                ]
-
-                        )
 
         """
+
 
         sf_inserts = []
         contacts = Contact.objects.all()
@@ -79,16 +43,18 @@ class Command(BaseCommand):
             sf_campaign_member = {
                 u'ContactID': contact.contact_id,
                 u'CampaignID': '7010m0000002pARAAY',
-                u'Initial_Sign_in_Date__c': '',
-                u'Most_recent_sign_in_date__c': '',
-                u'Completed_Training_Wheels_date__c': contact.completed_training_wheels_date,
-                u'Started_Assignment_date__c': contact.started_assignment_date,
-                u'Completed_Assignment_date__c': contact.completed_assignment_date,
-                u'Soft_Ask_Decision__c': contact.soft_ask_decision,
-                u'Soft_Ask_Decision_date__c': contact.soft_ask_decision_date,
-                u'Students_Pell_Grant__c': '',
-                u'Estimated_Enrollment__c': contact.estimated_enrollment,
-                u'latest_adoption_decision__c': contact.latest_adoption_decision,
+                u'accounts_uuid__c': contact.contact_id,
+                u'Status': 'Sent',
+            #    u'Initial_Sign_in_Date__c': '',
+            #    u'Most_recent_sign_in_date__c': '',
+            #    u'Completed_Training_Wheels_date__c': contact.completed_training_wheels_date,
+            #    u'Started_Assignment_date__c': contact.started_assignment_date,
+            #    u'Completed_Assignment_date__c': contact.completed_assignment_date,
+            #    u'Soft_Ask_Decision__c': contact.soft_ask_decision,
+            #    u'Soft_Ask_Decision_date__c': contact.soft_ask_decision_date,
+            #    u'Students_Pell_Grant__c': '',
+            #    u'Estimated_Enrollment__c': contact.estimated_enrollment,
+            #    u'latest_adoption_decision__c': contact.latest_adoption_decision,
                 }
             sf_inserts.append(sf_campaign_member)
 
@@ -98,7 +64,7 @@ class Command(BaseCommand):
 
             self.stdout.write(self.style.NOTICE(u"Sending {} inserts of CampaignMembers to salesforce.com ...".format(len(sf_inserts))))
             pprint(sf_inserts)
-            results = sf.bulk.CampaignMember.insert(sf_inserts)
+            results = sf.bulk.CampaignMember.upsert(sf_inserts, "accounts_uuid__c")
             # result should look like [{'errors': [], 'success': True, 'created': False, 'id': 'object_id_1'}]
 
             for result in results:
