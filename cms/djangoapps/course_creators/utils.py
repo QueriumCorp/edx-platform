41615202a101a2f2d9b84aeff4bf68487ad90ddb
Lawrence McDaniel <lpm0073@gmail.com>
2019-02-22 09:18:41 -0600

    scaffold utility functions to add course creators from oAuth

--- cms/djangoapps/course_creators/utils.py
+++ cms/djangoapps/course_creators/utils.py
@@ -7,6 +7,9 @@ Utilities for working with oAuth connections to openstax.org.
 from student import auth
 from student.roles import CourseCreatorRole
 from views import _add_user, update_course_creator_group
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 def grant_course_creator_status(user):
     """
@@ -15,6 +18,10 @@ def grant_course_creator_status(user):
     """
     _add_user(user, CourseCreator.GRANTED)
     update_course_creator_group(get_staff_user(), user, True)
+
+    full_name = user.first_name + u' ' + user.last_name
+    msg = u'{} has been granted course creator permissions in AM.'.format(full_name)
+    logger.info(msg)
 
 def get_staff_user():
     """
