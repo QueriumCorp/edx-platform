c41389aa386f9177567c9c1cfaeb771c8b38084d
Lawrence McDaniel <lpm0073@gmail.com>
2019-02-26 14:33:34 -0600

    add oauth context to home page view

--- lms/djangoapps/branding/views.py
+++ lms/djangoapps/branding/views.py
@@ -23,6 +23,23 @@ from openedx.core.djangoapps.site_configuration import helpers as configuration_
 from util.cache import cache_if_anonymous
 from util.json_request import JsonResponse
 
+# mcdaniel - feb-2019
+# imports for oAuth to openstax
+import third_party_auth
+from third_party_auth import pipeline
+from third_party_auth.decorators import xframe_allow_whitelisted
+from django.utils.translation import ugettext as _
+from openedx.core.djangoapps.external_auth.login_and_register import login as external_auth_login
+from openedx.core.djangoapps.external_auth.login_and_register import register as external_auth_register
+from student.helpers import get_next_url_for_login_page
+from django.contrib import messages
+from student.helpers import (
+    auth_pipeline_urls,
+    get_next_url_for_login_page
+)
+
+
+
 log = logging.getLogger(__name__)
 
 
@@ -74,7 +91,9 @@ def index(request):
 
     #  we do not expect this case to be reached in cases where
     #  marketing and edge are enabled
-    return student.views.index(request, user=request.user)
+    context = _get_login_context(request)
+    return render_to_response('index.html', context)
+    #return student.views.index(request, user=request.user)
 
 
 @ensure_csrf_cookie
@@ -309,3 +328,142 @@ def footer(request):
 
     else:
         return HttpResponse(status=406)
+
+
+"""
+    ----------------------------------------------------------------------
+    Code copied from
+    /edx/app/edxapp/edx-platform/lms/djangoapps/student_account/views.py
+"""
+def _third_party_auth_context(request, redirect_to, tpa_hint=None):
+    """Context for third party auth providers and the currently running pipeline.
+
+    Arguments:
+        request (HttpRequest): The request, used to determine if a pipeline
+            is currently running.
+        redirect_to: The URL to send the user to following successful
+            authentication.
+        tpa_hint (string): An override flag that will return a matching provider
+            as long as its configuration has been enabled
+
+    Returns:
+        dict
+
+    """
+    context = {
+        "currentProvider": None,
+        "providers": [],
+        "secondaryProviders": [],
+        "finishAuthUrl": None,
+        "errorMessage": None,
+        "registerFormSubmitButtonText": _("Create Account"),
+        "syncLearnerProfileData": False,
+        "pipeline_user_details": {}
+    }
+
+    if third_party_auth.is_enabled():
+        for enabled in third_party_auth.provider.Registry.displayed_for_login(tpa_hint=tpa_hint):
+            info = {
+                "id": enabled.provider_id,
+                "name": enabled.name,
+                "iconClass": enabled.icon_class or None,
+                "iconImage": enabled.icon_image.url if enabled.icon_image else None,
+                "loginUrl": pipeline.get_login_url(
+                    enabled.provider_id,
+                    pipeline.AUTH_ENTRY_LOGIN,
+                    redirect_url=redirect_to,
+                ),
+                "registerUrl": pipeline.get_login_url(
+                    enabled.provider_id,
+                    pipeline.AUTH_ENTRY_REGISTER,
+                    redirect_url=redirect_to,
+                ),
+            }
+            context["providers" if not enabled.secondary else "secondaryProviders"].append(info)
+
+        running_pipeline = pipeline.get(request)
+        if running_pipeline is not None:
+            current_provider = third_party_auth.provider.Registry.get_from_pipeline(running_pipeline)
+            user_details = running_pipeline['kwargs']['details']
+            if user_details:
+                context['pipeline_user_details'] = user_details
+
+            if current_provider is not None:
+                context["currentProvider"] = current_provider.name
+                context["finishAuthUrl"] = pipeline.get_complete_url(current_provider.backend_name)
+                context["syncLearnerProfileData"] = current_provider.sync_learner_profile_data
+
+                if current_provider.skip_registration_form:
+                    # As a reliable way of "skipping" the registration form, we just submit it automatically
+                    context["autoSubmitRegForm"] = True
+
+        # Check for any error messages we may want to display:
+        for msg in messages.get_messages(request):
+            if msg.extra_tags.split()[0] == "social-auth":
+                # msg may or may not be translated. Try translating [again] in case we are able to:
+                context['errorMessage'] = _(unicode(msg))  # pylint: disable=translation-of-non-string
+                break
+
+    return context
+
+def _external_auth_intercept(request, mode):
+    """Allow external auth to intercept a login/registration request.
+
+    Arguments:
+        request (Request): The original request.
+        mode (str): Either "login" or "register"
+
+    Returns:
+        Response or None
+
+    """
+    if mode == "login":
+        return external_auth_login(request)
+    elif mode == "register":
+        return external_auth_register(request)
+
+
+def _get_login_context(request):
+    # mcdaniel feb-2019: oAuth code from LMS login view
+    #   /edx/app/edxapp/edx-platform/common/djangoapps/student/views/login.py signin_user()
+    #   /edx/app/edxapp/edx-platform/lms/djangoapps/student_account/views.py  login_and_registration_form()
+    # ---------------------------------------------------
+    # Determine the URL to redirect to following login/registration/third_party_auth
+    redirect_to = get_next_url_for_login_page(request)
+
+    # Allow external auth to intercept and handle the request
+    ext_auth_response = _external_auth_intercept(request, "login")
+    if ext_auth_response is not None:
+        return ext_auth_response
+
+    third_party_auth_error = None
+    for msg in messages.get_messages(request):
+        if msg.extra_tags.split()[0] == "social-auth":
+            # msg may or may not be translated. Try translating [again] in case we are able to:
+            third_party_auth_error = _(text_type(msg))  # pylint: disable=translation-of-non-string
+            break
+
+
+    # Our ?next= URL may itself contain a parameter 'tpa_hint=x' that we need to check.
+    # If present, we display a login page focused on third-party auth with that provider.
+    third_party_auth_hint = None
+    third_party_auth = _third_party_auth_context(request, redirect_to, third_party_auth_hint)
+    context = {
+        ## mcdaniel: preserved from THIS template
+        ## ----------------------
+        'login_redirect_url': redirect_to,  # This gets added to the query string of the "Sign In" button in the header
+        ## ----------------------
+
+        # Bool injected into JS to submit form if we're inside a running third-
+        # party auth pipeline; distinct from the actual instance of the running
+        # pipeline, if any.
+        'pipeline_running': 'true' if pipeline.running(request) else 'false',
+        'pipeline_url': auth_pipeline_urls(pipeline.AUTH_ENTRY_LOGIN, redirect_url=redirect_to),
+        'third_party_auth': third_party_auth,
+        'third_party_auth_hint': third_party_auth_hint or '',
+        'third_party_auth_error': third_party_auth_error,
+
+    }
+    # end of code copied from common and lms login views
+    # ---------------------------------------------------
+    return context
