84c9949e3f43728c21a6b7a5df36a2aa0595d538
Lawrence McDaniel <lpm0073@gmail.com>
2019-03-14 12:44:00 -0600

    is_faculty() add no_faculty_info and pending_faculty as permitted status codes

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -2420,8 +2420,9 @@ considered a valid course_creator if this field = faculty_confirmed
 """
 def is_faculty(user):
     profile = UserProfile.objects.get(user=user)
-    log.info('evaluate_course_creator_status() - {}'.format(profile.faculty_status))
-    return profile.faculty_status == 'confirmed_faculty'
+    log.info('is_faculty() - {}'.format(profile.faculty_status))
+    log.info('is_faculty() - FIX NOTE: allowing confirmed_faculty, pending_faculty, no_faculty_info')
+    return profile.faculty_status == 'confirmed_faculty' or profile.faculty_status == 'pending_faculty' or profile.faculty_status == 'no_faculty_info'
 
 @receiver(user_logged_in)
 def log_successful_login(sender, request, user, **kwargs):  # pylint: disable=unused-argument
