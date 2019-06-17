f8c760d6a1300e10bfcf6b3d930fe159afe044a9
Lawrence McDaniel <lpm0073@gmail.com>
2019-06-17 12:10:28 -0700

    New salesforce training wheels fields

--- cms/djangoapps/openstax_integrator/salesforce/models.py
+++ cms/djangoapps/openstax_integrator/salesforce/models.py
@@ -62,6 +62,17 @@ class Contact(models.Model):
     started_assignment_date = models.DateTimeField(blank=True, null=True)
     completed_assignment_date = models.DateTimeField(blank=True, null=True)
 
+    # mcdaniel jun-2019
+    # Expanded Training Wheels fields from Allison/Denver
+    # https://docs.google.com/spreadsheets/d/1--9XMFC0bouIoR3vGX8ipVzEtlgtXZ_2kd8knwVPZVo/edit?ts=5d015789#gid=0
+    Completed_super_training_wheel_1 = models.DateTimeField(blank=True, null=True)
+    Downloaded_GSG__c = models.DateTimeField(blank=True, null=True)
+    Watched_video__c = models.DateTimeField(blank=True, null=True)
+    Completed_AM_page_tips__c = models.DateTimeField(blank=True, null=True)
+    Created_a_course__c = models.DateTimeField(blank=True, null=True)
+    Completed_building_assignments_training__c = models.DateTimeField(blank=True, null=True)
+    Completed_how_to_set_up_grading_page_tip__c = models.DateTimeField(blank=True, null=True)
+
     # TBD 3rd party system
     soft_ask_decision = models.CharField(max_length=255, blank=True, null=True)
     soft_ask_decision_date = models.DateTimeField(blank=True, null=True)
