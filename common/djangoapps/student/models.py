c19b864d7c0022939954d0efc7a5ab15d0772007
Lawrence McDaniel <lpm0073@gmail.com>
2019-06-22 09:36:57 -0600

    restrict is_faculty() to confirmed_faculty

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -2421,8 +2421,9 @@ considered a valid course_creator if this field = faculty_confirmed
 def is_faculty(user):
     profile = UserProfile.objects.get(user=user)
     log.info('is_faculty() - {}'.format(profile.faculty_status))
-    log.info('is_faculty() - FIX NOTE: allowing confirmed_faculty, pending_faculty, no_faculty_info')
-    return profile.faculty_status == 'confirmed_faculty' or profile.faculty_status == 'pending_faculty' or profile.faculty_status == 'no_faculty_info'
+    #log.info('is_faculty() - FIX NOTE: allowing confirmed_faculty, pending_faculty, no_faculty_info')
+    #return profile.faculty_status == 'confirmed_faculty' or profile.faculty_status == 'pending_faculty' or profile.faculty_status == 'no_faculty_info'
+    return profile.faculty_status == 'confirmed_faculty'
 
 @receiver(user_logged_in)
 def log_successful_login(sender, request, user, **kwargs):  # pylint: disable=unused-argument
