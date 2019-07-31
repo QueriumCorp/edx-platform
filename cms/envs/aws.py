2bb26f2eec47b36caac699d6e7ccfdde1e041ed2
Lawrence McDaniel <lpm0073@gmail.com>
2019-07-31 17:21:23 -0400

    testing

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -6,7 +6,8 @@ This is the default template for our main set of AWS servers.
 #           {CLIENT} = 'dev'
 #           {CLIENT}.roverbyopenstax.org becomes: dev.roverbyopenstax.org
 def rover_env_token(token, default=None):
-    return ENV_TOKENS.get(token, default).replace('{CLIENT}', ROVER_CLIENT_CODE)
+    str = string(ENV_TOKENS.get(token, default))
+    return str.replace('{CLIENT}', ROVER_CLIENT_CODE)
 
 # We intentionally define lots of variables that aren't used, and
 # want to import all variables from base settings files
--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -4,7 +4,8 @@
 #           {CLIENT} = 'dev'
 #           {CLIENT}.roverbyopenstax.org becomes: dev.roverbyopenstax.org
 def rover_env_token(token, default=None):
-    return ENV_TOKENS.get(token, default).replace('{CLIENT}', ROVER_CLIENT_CODE)
+    str = string(ENV_TOKENS.get(token, default))
+    return str.replace('{CLIENT}', ROVER_CLIENT_CODE)
 
 """
 This is the default template for our main set of AWS servers.
