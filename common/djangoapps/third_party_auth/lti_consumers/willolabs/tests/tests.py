df9f6aefab145e2c541bf623532033b6607821b8
Lawrence McDaniel <lpm0073@gmail.com>
2019-12-17 15:46:33 -0500

    setup tests

--- common/djangoapps/third_party_auth/lti_consumers/willolabs/tests/tests.py
+++ common/djangoapps/third_party_auth/lti_consumers/willolabs/tests/tests.py
@@ -5,30 +5,37 @@ Unit tests for third_party_auth LTI - Willo Labs Grade Sync
 """
 
 from __future__ import absolute_import
-import unittest
+import os
 import json
 
 #import requests
 #from oauthlib.common import Request
 #from third_party_auth.lti import LTI_PARAMS_KEY, LTIAuthBackend
+from django.test import TestCase
 
 from third_party_auth.tests.testutil import ThirdPartyAuthTestMixin
 from django.contrib.auth import get_user_model
-from third_party_auth.lti.willolabs.provisioners import CourseProvisioner
-from third_party_auth.lti.willolabs.utils import is_willo_lti
+from third_party_auth.lti_consumers.willolabs.provisioners import CourseProvisioner
+from third_party_auth.lti_consumers.willolabs.utils import is_willo_lti
 
 LTI_PARAMS_KEY = 'tpa-lti-params'
 LTI_PARAMS_JSON_FILE = 'data/tpa-lti-params-willo-canvas-learner.json'
 USER_MODEL = get_user_model()
 
-class UnitTestLTIWilloLabGradeSync(unittest.TestCase, ThirdPartyAuthTestMixin):
+class UnitTestLTIWilloLabGradeSync(TestCase):
     """
     Unit tests for third_party_auth LTI auth providers
     """
+    def __init__(self):
+        print('setup()')
+        self.PATH = os.path.dirname(__file__) + '/'
+    
+    def runTest(self):
+        print('runTest()')
 
     def test_course_provisioner_instantiation(self):
         user = USER_MODEL.objects.get(username="mcdaniel")
-        with open(LTI_PARAMS_JSON_FILE) as json_file:
+        with open(self.PATH + LTI_PARAMS_JSON_FILE) as json_file:
             data = json.load(json_file)
         lti_params = data[LTI_PARAMS_KEY]
 
@@ -37,7 +44,7 @@ class UnitTestLTIWilloLabGradeSync(unittest.TestCase, ThirdPartyAuthTestMixin):
 
     def test_is_willo_lti(self):
         user = USER_MODEL.objects.get(username="mcdaniel")
-        with open(LTI_PARAMS_JSON_FILE) as json_file:
+        with open(self.PATH + LTI_PARAMS_JSON_FILE) as json_file:
             data = json.load(json_file)
         lti_params = data[LTI_PARAMS_KEY]
 
@@ -45,7 +52,7 @@ class UnitTestLTIWilloLabGradeSync(unittest.TestCase, ThirdPartyAuthTestMixin):
         self.assertEquals(is_willo, True)
 
     def test_is_not_willo_lti(self):
-        with open(LTI_PARAMS_JSON_FILE) as json_file:
+        with open(self.PATH + LTI_PARAMS_JSON_FILE) as json_file:
             data = json.load(json_file)
         lti_params = data[LTI_PARAMS_KEY]
 
