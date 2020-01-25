68a07552f72845f6babf05c99ba9586a88a71cd6
Lawrence McDaniel <lpm0073@gmail.com>
2020-01-25 10:32:14 -0500

    documentation

--- openedx/core/djangoapps/content/course_overviews/models.py
+++ openedx/core/djangoapps/content/course_overviews/models.py
@@ -618,8 +618,10 @@ class CourseOverview(TimeStampedModel):
         # settings.DEFAULT_COURSE_ABOUT_IMAGE_URL if they didn't specify one.
         raw_image_url = self.course_image_url
 
+        # FIX NOTE: DELETE ME!!!!
         # mcdaniel oct-2019: hack to fix the missing course cards from the LMS
         # home screen. 
+        #
         if "precalculus" in self.display_name.lower():
             raw_image_url = 'https://cdn.roverbyopenstax.org/images/precalculus.svg'
         elif "trig" in self.display_name.lower():
