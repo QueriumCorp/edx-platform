aa1367a8e11ec49c1c62297e1b10a334eac33e6a
Lawrence McDaniel <lpm0073@gmail.com>
2020-01-27 19:47:11 -0500

    remove stupid validation

--- common/djangoapps/third_party_auth/models.py
+++ common/djangoapps/third_party_auth/models.py
@@ -214,8 +214,10 @@ class ProviderConfig(ConfigurationModel):
     def clean(self):
         """ Ensure that either `icon_class` or `icon_image` is set """
         super(ProviderConfig, self).clean()
-        if bool(self.icon_class) == bool(self.icon_image):
-            raise ValidationError('Either an icon class or an icon image must be given (but not both)')
+        # mcdaniel jan-2020
+        # breaks the django update screen
+        #if bool(self.icon_class) == bool(self.icon_image):
+        #    raise ValidationError('Either an icon class or an icon image must be given (but not both)')
 
     @property
     def provider_id(self):
