c31c22ea7433344b43e82210880d6b53ebe1ac09
Lawrence McDaniel <lpm0073@gmail.com>
2020-08-24 12:04:38 -0500

    new command to bulk enable/disable LTI Grade Sync

---
+++ common/djangoapps/third_party_auth/lti_consumers/management/commands/bulk_toggle.py
@@ -0,0 +1,55 @@
+"""
+  Written by:   mcdaniel
+                lpm0073@gmail.com
+                https://lawrencemcdaniel.com
+
+  Date:         Jan-2020
+
+  LTI Grade Sync.
+  Command line tool to bulk enable/disable course-level LTI Grade Sync
+
+  Usage:
+    sudo -H -u edxapp bash
+    cd ~
+    source edxapp_env
+    source venvs/edxapp/bin/activate
+    cd edx-platform
+    python manage.py lms bulk_toggle enable
+"""
+from django.core.management.base import BaseCommand
+
+# our stuff
+from common.djangoapps.third_party_auth.lti_consumers.models import LTIInternalCourse
+
+VALID_COMMANDS = ['enable', 'disable']
+
+class Command(BaseCommand):
+    help = u"LTI Grade Sync. Utilities for creating and maintaining LTI Configurations."
+
+    def add_arguments(self, parser):
+      parser.add_argument(
+            'command',
+            type=str,
+            help='valid commands: enable, disable'
+            )
+
+
+    def handle(self, *args, **kwargs):
+        cmd = kwargs['command'].lower()
+
+        if cmd not in VALID_COMMANDS:
+            print('Valid commands include: {commands}'.format(
+                commands=json.dumps(VALID_COMMANDS)
+            ))
+            return
+
+        enabled = cmd == 'enable'
+
+        lti_internal_courses = LTIInternalCourse.objects.all()
+        for course in lti_internal_courses:
+            print('{cmd} {course_key}'.format(
+                cmd=cmd,
+                course_key=str(course.course_fk)
+            ))
+            course.enabled = enabled
+            course.save()
