0840a0c291ae3d5181afd69a86ab4a266339dde1
Lawrence McDaniel <lpm0073@gmail.com>
2020-09-03 12:26:02 -0500

    switch name of api to v1

--- lms/djangoapps/querium/grades_api/urls.py
+++ lms/djangoapps/querium/grades_api/urls.py
@@ -5,28 +5,9 @@ Grades API URLs.
 from django.conf import settings
 from django.conf.urls import include, url
 
-"""
-from lms.djangoapps.grades.api import views
-
-urlpatterns = [
-    url(
-        r'^v0/course_grade/{course_id}/users/$'.format(
-            course_id=settings.COURSE_ID_PATTERN,
-        ),
-        views.UserGradeView.as_view(), name='user_grade_detail'
-    ),
-    url(
-        r'^v0/courses/{course_id}/policy/$'.format(
-            course_id=settings.COURSE_ID_PATTERN,
-        ),
-        views.CourseGradingPolicy.as_view(), name='course_grading_policy'
-    ),
-    url(r'^v1/', include('grades.api.v1.urls', namespace='v1')),
-]
-"""
 
 # mcdaniel feb-2020: only include grades api if feature flag is set.
 if settings.ROVER_ENABLE_GRADES_API:
     urlpatterns = [
-        url(r'^v2/', include('querium.grades_api.v2.urls', namespace='v2'))
+        url(r'^v1/', include('querium.grades_api.v1.urls', namespace='v2'))
     ]
