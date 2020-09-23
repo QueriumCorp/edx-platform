d90c3ce128090a459b546b7f2d205854f5a9b2a7
Lawrence McDaniel <lpm0073@gmail.com>
2020-09-23 13:38:42 -0500

    new querium Course Management app

--- cms/djangoapps/querium/querium_utilities/management/commands/querium_export.py
+++ cms/djangoapps/querium/querium_utilities/management/commands/querium_export.py
@@ -11,6 +11,8 @@ At present, it differs from Studio exports in several ways:
   (i.e. ascii) version of the course_key, rather than the word "course".
 * It only supports the export of courses.  It does not export libraries.
 """
+from textwrap import dedent
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
@@ -34,13 +36,23 @@ class Command(BaseCommand):
     help = dedent(__doc__).strip()
 
     def add_arguments(self, parser):
-        parser.add_argument('course_id')
-        parser.add_argument('username')
+        parser.add_argument(
+            'course_id',
+            help=u"a valid CourseKey string. Example: course-v1:KNCL+DemoX_3930+Fall2020_KNCL_Test"
+        )
+        parser.add_argument(
+            'username',
+            help=u"a valid active Rover username with admin/superuser privileges. Example: admin"
+            )
 
     def handle(self, *args, **options):
         course_id = options['course_id']
         username = options['username']
 
-    user = User.objects.get(username=username)
-    language_code = 'en'
-    export_olx.delay(user.id, course_id, language_code)
+        print('Course_id: {course_id}, username: {username}'.format(
+            course_id=course_id,
+            username=username
+        ))
+        user = User.objects.get(username=username)
+        language_code = 'en'
+        export_olx.delay(user.id, course_id, language_code)
