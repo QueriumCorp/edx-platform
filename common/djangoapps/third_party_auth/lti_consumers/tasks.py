e7db65cd69774ce6c7db877b262ee28a3d636fdf
Lawrence McDaniel <lpm0073@gmail.com>
2021-01-22 09:50:17 -0600

    add a flag CALSTATELA_MIDTERM3_PATCH to filter out patch code from 2020 mid term 3 at calstatela

--- common/djangoapps/third_party_auth/lti_consumers/tasks.py
+++ common/djangoapps/third_party_auth/lti_consumers/tasks.py
@@ -50,7 +50,7 @@ from lms.djangoapps.grades.subsection_grade_factory import SubsectionGradeFactor
 
 # mcdaniel dec-2020
 # Calstatela Fall 2020 midterm3 patch
-CALSTATELA_MIDTERM3_PATCH = True
+CALSTATELA_MIDTERM3_PATCH = False
 if CALSTATELA_MIDTERM3_PATCH:
     try:
         from .patches.calstatela_2020midterm3_patch_001 import (
